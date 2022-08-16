a2da87e984fb0848dc798805b3dbf93b4783e0d5
dbc2201 <divyanshb22@gmail.com>
2022-08-16 22:33:45 +0530

    added dependency for JUnit 5 in the pom.xml; created a test class called MainTest.java

---
+++ src/test/java/io/github/dbc/MainTest.java
@@ -0,0 +1,15 @@
+package io.github.dbc;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void main() {
+    }
+}
